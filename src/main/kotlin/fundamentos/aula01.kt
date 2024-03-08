d13e0c4f3a116201a77d205b5aed27ab6361df55
Alanpds <alan.pinheirodossantos@gmail.com>
2024-03-08 19:02:15 -0300

    comentários

--- src/main/kotlin/fundamentos/aula01.kt
+++ src/main/kotlin/fundamentos/aula01.kt
@@ -1,10 +1,6 @@
 package fundamentos
-
-import funcoes.soma
-
 fun main() {
     print("primeiro")
     println(" programa!");
     println("agora com github");
-    println(soma(5))
 }
---
+++ src/main/kotlin/fundamentos/comentarios.kt
@@ -0,0 +1,8 @@
+package fundamentos
+
+//comentário de uma linha só
+
+/*
+comentário de múltiplas linhas,
+usado normalmente para códigos maiores
+*/
